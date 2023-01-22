5d4dc24bc38ce6db82b9a3012d0d9eeb349ae13c
lightiichen <felix961308@gmail.com>
2023-01-22 10:32:09 +0800

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,26 @@
 import './globals.css'
+import styles from "@/app/layout.module.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
+	return (
+		<html lang="en">
+		{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+		<head/>
+
+
+		<body className={styles.baseLayout}>
+
+				{children}
+
+		</body>
+
+		</html>
+	)
 }
